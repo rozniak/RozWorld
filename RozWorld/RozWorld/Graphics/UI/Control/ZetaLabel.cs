7af29d326677c743f09c5eb32a0a1cdf50063bd6
rozniak <roryfewell@hotmail.com>
2015-11-25 21:34:02 +0000

    Will work on FontProvider next

--- RozWorld/RozWorld/Graphics/UI/Control/ZetaLabel.cs
+++ RozWorld/RozWorld/Graphics/UI/Control/ZetaLabel.cs
@@ -9,15 +9,93 @@
  * Sharing, editing and general licence term information can be found inside of the "LICENCE.MD" file that should be located in the root of this project's directory structure.
  */
 
+using OpenGL;
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+
 namespace RozWorld.Graphics.UI.Control
 {
-    public class ZetaLabel
+    public class ZetaLabel : ControlSkeleton
     {
-        // This will be coded up later on
+        private Vector4 _DefaultColour;
+        public Vector4 DefaultColour
+        {
+            get { return this._DefaultColour; }
+            set
+            {
+                this._DefaultColour = value;
+                UpdateDrawInstruction("colour");
+            }
+        }
+
+        private string _Text;
+        public string Text
+        {
+            get { return this._Text; }
+            set
+            {
+                this._Text = value;
+                UpdateDrawInstruction("text");
+            }
+        }
+
+
+        private FontType _Font;
+        public FontType Font
+        {
+            get { return this._Font; }
+            set
+            {
+                this._Font = value;
+                UpdateDrawInstruction("text");
+            }
+        }
+
+
+        public event KeyEventHandler OnKeyDown;
+        public event KeyEventHandler OnKeyUp;
+        public event SenderEventHandler OnMouseDown;
+        public event SenderEventHandler OnMouseEnter;
+        public event SenderEventHandler OnMouseLeave;
+        public event SenderEventHandler OnMouseUp;
+
+
+        public ZetaLabel(GameWindow parentWindow)
+        {
+            this.ParentWindow = parentWindow;
+            this._DefaultColour = VectorColour.OpaqueWhite;
+            this.Position = new Vector2(0, 0);
+            this._Font = FontType.ChatFont;
+            this._Text = "";
+            this.ZIndex = 1;
+        }
+
+
+        /// <summary>
+        /// Implementation of the base draw instruction updating method.
+        /// </summary>
+        /// <param name="updateKey">The update key representing the desired update.</param>
+        public override void UpdateDrawInstruction(string updateKey)
+        {
+            // TODO: Sort this out with FontProvider
+            switch (updateKey)
+            {
+                case "position":
+                    break;
+
+                case "text":
+                    break;
+
+                case "visible":
+                    break;
+
+                case "colour":
+                    break;
+            }
+        }
     }
 }
