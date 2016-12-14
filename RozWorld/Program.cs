f09bfb753ecd75c594254552e3562cc241cabc9e
Rory Fewell <roryfewell@hotmail.com>
2016-12-14 20:58:53 +0000

    Some initial work on getting the client to load
    This doesn't do much obviously! But it is the beginning of something
    cool hopefully :neckbeard:

--- RozWorld/Program.cs
+++ RozWorld/Program.cs
@@ -9,6 +9,9 @@
  * Sharing, editing and general licence term information can be found inside of the "LICENCE.MD" file that should be located in the root of this project's directory structure.
  */
 
+using Oddmatics.RozWorld.API.Generic;
+using System;
+
 namespace Oddmatics.RozWorld.Client
 {
     class Program
@@ -17,7 +20,24 @@ namespace Oddmatics.RozWorld.Client
         {
             var client = new RwClient();
 
-            client.Run(); // This will throw an exception for now
+            client.Logger = new GameLogger();
+            RwCore.Client = client;
+
+            if (client.Run())
+                client.Logger.Out("Game ran successfully.", LogLevel.Info);
+            else
+                client.Logger.Out("Game failed to run successfully! Review the log for any errors.",
+                    LogLevel.Info);
+
+            Console.ReadKey(true); // Temporary
+        }
+    }
+
+    internal sealed class GameLogger : ILogger
+    {
+        public void Out(string message, LogLevel level, bool colours = true)
+        {
+            Console.WriteLine("[" + DateTime.Now.ToString() + "] " + level + ":  " + message);
         }
     }
 }
--- RozWorld/RwClient.cs
+++ RozWorld/RwClient.cs
@@ -13,12 +13,26 @@ using Oddmatics.RozWorld.API.Client;
 using Oddmatics.RozWorld.API.Client.Input;
 using Oddmatics.RozWorld.API.Client.Interface;
 using Oddmatics.RozWorld.API.Generic;
+using Oddmatics.Util.IO;
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
 
 namespace Oddmatics.RozWorld.Client
 {
-    public class RwClient :IRwClient
+    public class RwClient : IRwClient
     {
+        #region Path Constants
+
+        /// <summary>
+        /// The renderers directory.
+        /// </summary>
+        public static string DIRECTORY_RENDERERS = Directory.GetCurrentDirectory() + @"\renderers";
+
+        #endregion
+
+
         public string ClientName { get { return "Vanilla RozWorld Client"; } }
         public string ClientVersion { get { return "0.01"; } }
         public IInputHandler Input { get { throw new System.NotImplementedException(); } }
@@ -32,6 +46,8 @@ namespace Oddmatics.RozWorld.Client
 
 
         private bool HasStarted;
+        private List<Type> Renderers;
+        private Renderer ActiveRenderer;
 
 
         /// <summary>
@@ -50,8 +66,56 @@ namespace Oddmatics.RozWorld.Client
                 throw new InvalidOperationException("RwClient.Start: Client is already started.");
 
             Logger.Out("RozWorld client starting...", LogLevel.Info);
+            Logger.Out("Initialising directories...", LogLevel.Info);
 
-            // TODO: More of this of course
+            FileSystem.MakeDirectory(DIRECTORY_RENDERERS);
+
+            // TODO: LOAD CONFIGURATION FILE HERE!!!
+
+            // Load renderers
+            Logger.Out("Loading renderers...", LogLevel.Info);
+
+            Renderers = new List<Type>();
+
+            foreach (string file in Directory.GetFiles(DIRECTORY_RENDERERS))
+            {
+                try
+                {
+                    Assembly assembly = Assembly.LoadFrom(file);
+                    Type[] detectedObjects = assembly.GetTypes();
+
+                    foreach (var detectedObject in detectedObjects)
+                    {
+                        if (detectedObject.IsAssignableFrom(typeof(Renderer)))
+                            Renderers.Add(detectedObject);
+                    }
+                }
+                catch (ReflectionTypeLoadException reflectionEx)
+                {
+                    Logger.Out("An error occurred trying to enumerate the types inside of the library \"" +
+                        Path.GetFileName(file) + "\", renderers from this library cannot be loaded. It may" +
+                        "have been built with a different version of the RozWorld API.", LogLevel.Error);
+                }
+                catch (Exception ex)
+                {
+                    Logger.Out("An error occurred trying to load library \"" + Path.GetFileName(file) +
+                        "\", renderers from this library cannot be loaded. The exception that occurred " +
+                        "reported the following:\n" + ex.Message + "\nStack:\n" + ex.StackTrace, LogLevel.Error);
+                }
+            }
+
+            // TODO: Replace this bit so that renderer choice will be loaded from configs
+            if (Renderers.Count == 0)
+            {
+                Logger.Out("No renderers were loaded! Cannot continue.", LogLevel.Fatal);
+                return false;
+            }
+
+            ActiveRenderer = (Renderer)Activator.CreateInstance(Renderers[0]); // This will be replaced - dw for now
+
+            ActiveRenderer.Initialise();
+
+            // Run game here!
 
             return true;
         }
