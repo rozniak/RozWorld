bfe9edb524539b20a080640efbb51f8f25473e73
Rory Fewell <roryfewell@hotmail.com>
2017-02-22 23:45:06 +0000

    Test shader updates

--- RozWorld/Shaders/fragment.glsl
+++ RozWorld/Shaders/fragment.glsl
@@ -1,12 +1,15 @@
 #version 330 core
 
+in vec2 UV;
 
 out vec3 Colour;
 
 uniform float fTime;
+uniform sampler2D TextureSampler;
 
 
 void main()
 {
-    Colour = vec3(abs(sin(fTime)), abs(cos(fTime)), abs(sin(fTime / 5f)));
+    //Colour = vec3(abs(sin(fTime)), abs(cos(fTime)), abs(sin(fTime / 5f)));
+    Colour = texture(TextureSampler, UV).rgb;
 }
--- RozWorld/Shaders/vertex.glsl
+++ RozWorld/Shaders/vertex.glsl
@@ -1,9 +1,14 @@
 #version 330 core
 
 layout(location = 0) in vec3 VertexPosition;
+layout(location = 1) in vec2 VertexUV;
+
+out vec2 UV;
 
 void main()
 {
     gl_Position.xyz = VertexPosition;
-	gl_Position.w = 1.0;
+    gl_Position.w = 1.0;
+    
+    UV = VertexUV;
 }
